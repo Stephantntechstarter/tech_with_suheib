08b04179732baa13bea6244a4c9f0a6f5df1a56c
Stephantntechstarter <stephan.schulz@tn.techstarter.de>
2025-04-01 11:33:47 +0200

    Delete assignment/website/shopping_list directory

--- assignment/website/shopping_list/js/modules/editMode.js
+++
@@ -1,9 +0,0 @@
-function enterEditMode(li, textSpan) {
-  li.classList.add("editing");
-  textSpan.style.display = "none";
-
-  const editContainer = document.createElement("div");
-}
-
-function createListItem(itemData) {
-}
